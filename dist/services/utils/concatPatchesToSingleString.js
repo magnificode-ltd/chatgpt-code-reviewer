c7290d9431a33a5443c9e230cee3658733ef7d52
Andrew Husiev <andrew@magnificode.net>
2023-05-17 15:32:49 -0700

    Add fixes

--- dist/services/utils/concatPatchesToSingleString.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const concatPatchesToSingleString = (files) => files.map(({ filename, patch }) => `${filename}\n${patch}\n`).join('');
-exports.default = concatPatchesToSingleString;
