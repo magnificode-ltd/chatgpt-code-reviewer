15501adf71781bb2042a0cc13858a4af57a4b7fa
Andrew Husiev <andrew@magnificode.net>
2023-05-17 06:57:21 -0700

    Add fixes

--- dist/config/promptsConfig.js
+++ dist/config/promptsConfig.js
@@ -9,6 +9,6 @@ var Prompt;
 exports.Prompt = Prompt;
 const promptsConfig = {
     [Prompt.CHECK_PATCH]: 'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.',
-    [Prompt.SYSTEM_PROMPT]: 'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices. Additionally, split suggestions by filenames using a pattern `@@ filename @@`',
+    [Prompt.SYSTEM_PROMPT]: 'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.\nStart every suggestion with path to the file wrapped with @@',
 };
 exports.default = promptsConfig;
--- src/config/promptsConfig.ts
+++ src/config/promptsConfig.ts
@@ -7,7 +7,7 @@ const promptsConfig: { [key in Prompt]: string } = {
   [Prompt.CHECK_PATCH]:
     'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.',
   [Prompt.SYSTEM_PROMPT]:
-    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices. Additionally, split suggestions by filenames using a pattern `@@ filename @@`',
+    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.\nStart every suggestion with path to the file wrapped with @@',
 };
 
 export default promptsConfig;
