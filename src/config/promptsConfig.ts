869118a8e483652858bafa57e9b69ec08ecf6190
Andrew Husiev <andrew@magnificode.net>
2023-05-18 14:05:02 -0700

    Add fixes

--- src/config/promptsConfig.ts
+++ src/config/promptsConfig.ts
@@ -1,11 +1,8 @@
 enum Prompt {
-  CHECK_PATCH,
   SYSTEM_PROMPT,
 }
 
 const promptsConfig: { [key in Prompt]: string } = {
-  [Prompt.CHECK_PATCH]:
-    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.',
   [Prompt.SYSTEM_PROMPT]:
     'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
 };
